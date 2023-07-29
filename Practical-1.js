068a575fc956243235795289a620299f3b258c21
Vatsal Rathod <122249051+vatsalrathodd@users.noreply.github.com>
2023-07-29 22:33:34 +0530

    Add files via upload

---
+++ Practical-1.js
@@ -0,0 +1,12 @@
+const StringName ="Vatsal"
+ const Roll = 116
+const Admission = true
+
+console.log(StringName); console.log(Roll); console.log(Admission);
+
+
+console.log("Running the function"); const fun = (a,b) => {
+return a + b
+}
+
+console.log(fun(2,3));
